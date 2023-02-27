375161dcc927993964fed439a9003790bb1d8194
ALEX ALEJANDRO OVIEDO GONZALEZ <software.development.git@gmail.com>
2023-02-27 05:40:04 -0500

    Ejemplo de Fibonacci

---
+++ Fibonacci/Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+	public static void main (String [] args){
+		
+		Fibonacci calculo = new Fibonacci (9);
+		System.out.println(calculo.getFibonacci());
+		//Factorial fact = new Factorial(10);
+		//System.out.println(fact.getFactorial());
+	}
+}
